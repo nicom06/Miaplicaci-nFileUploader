d9bca5ead971d579cb44d6016e3ccee7ca230ac5
nicom06 <119193168+nicom06@users.noreply.github.com>
2026-04-20 22:21:46 -0500

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-import streamli as st
+import streamlit as st
+import pandas as pd
+
+
+st.title("Manejo de Dataframes")
+st.sidebar.title("Herramientas")
+
+
+archivo = st.sidebar.file_uploader("Seleccione su archivo")
+
+datos = pd.read_csv(archivo)
+
+st.write(datos)
